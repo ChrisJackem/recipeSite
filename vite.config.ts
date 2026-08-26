198e8a6454fe3514499a4e26cd3ca5ad72b94cfa
Chris Jackem <chrisjackem@gmail.com>
2026-08-26 11:33:43 -0400

    Firebase Deployed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change 'build' to your desired directory name
+    emptyOutDir: true // Ensures the directory is cleared before building
+  }
 })
